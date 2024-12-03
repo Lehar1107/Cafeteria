73fc77ed8ec3da01f1e662866754bdd2b6520674
LEhar1107 <agrawallehar66@gmail.comgit config --global user.email agrawallehar66@gmail.com>
2024-12-03 10:52:06 +0530

    Caffe Website

---
+++ script.js
@@ -0,0 +1,49 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+menuOpenButton.addEventListener("click" , () => {
+    //Toggle menu visibility
+    document.body.classList.toggle("show-mobile-menu")
+});
+//menu close button cross sign
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+//menu when nav link is clicked
+navLinks.forEach(link => { 
+    link.addEventListener("click", () => menuOpenButton.click());
+})
+
+// Import Swiper
+//import Swiper from 'swiper';
+
+// Initialize Swiper
+const swiper = new Swiper('.slider-wrapper', {
+    // Optional parameters
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 25,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    breakpoints: {
+        0: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    }
+  });
